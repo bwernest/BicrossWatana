28d547b3cd3662389083c1e9897f390d5a5d6abc
bwernest <ernest.laprelleboucher@gmail.com>
2025-01-26 17:57:22 +0100

    First files

---
+++ MMM_exec.bat
@@ -0,0 +1,5 @@
+call MiniMouseMacro.exe MMM_instructions.mmmacro
+echo lol
+timeout /t 5 /nobreak > nul
+echo mdr
+exit
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import subprocess
 
 
+
+subprocess.run("MMM_exec.bat", shell=True)
+
+subprocess.run(['taskkill', '/f', '/im', "MiniMouseMacro.exe"], shell=True)
