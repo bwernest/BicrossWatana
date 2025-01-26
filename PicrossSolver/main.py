48bf810e773b78f75a4d35718dd0be28e7cda992
bwernest <ernest.laprelleboucher@gmail.com>
2025-01-26 19:25:53 +0100

    Easy lines solved

--- PicrossSolver/main.py
+++ PicrossSolver/main.py
@@ -4,6 +4,7 @@
 """
 """___Module________________________________________________________________"""
 
+import matplotlib.pyplot as plt
 import numpy as np
 
 """___Data__________________________________________________________________"""
@@ -20,8 +21,9 @@ class PicrossSolver() :
     tips : dict[str:list[str]] = {}
     draw : np.array
 
-    def __init__(self, path : str) -> None :
+    def __init__(self, path:str) -> None :
         self.import_from_txt(path)
+        self.draw = np.zeros((self.line, self.row), dtype=str)
 
     def import_from_txt(self, path:str) -> None :
         with open(path, "r") as txt :
@@ -40,11 +42,53 @@ class PicrossSolver() :
             elif key[0] == "R" :
                 self.row += 1
 
+    def solve(self) -> None :
+        self.easy_tips()
+
+    def easy_tips(self) -> None :
+        for key, tip in self.tips.items() :
+            tip_length = self.get_tip_length(tip)
+            if key[0] == "L" and tip_length == self.line :
+                self.fill_line(key, tip)
+            elif key[0] == "R" and tip_length == self.row :
+                self.fill_line(key, tip)
+
+    def get_tip_length(self, tip:str) -> int :
+        lenT = len(tip)
+        blocs = sum(int(eval(tip[i][1:])) for i in range(lenT))
+        spaces = 0
+        for bloc in range(lenT-1) :
+            if tip[bloc][0] == tip[bloc+1][0] :
+                spaces += 1
+        return blocs + spaces
+
+    def fill_line(self, key:str, tip:str) -> None :
+        line = np.zeros(self.row, dtype=str)
+        shift = 0
+        for bloc in self.tips[key] :
+            color = bloc[0]
+            length = int(eval(bloc[1:]))
+            for i in range(shift, length+shift) :
+                line[i] = color
+            shift += length + 1
+        if key[0] == "L" :
+            self.draw[int(key[1:])] = line
+        elif key[0] == "R" :
+            self.draw[:, int(key[1:])] = line
+    
+    def show(self) -> None :
+        disp_draw = np.zeros((self.line, self.row), dtype=int)
+        for line in range(self.line) :
+            for row in range(self.row) :
+                if self.draw[line, row] != "" :
+                    disp_draw[line, row] = ord(self.draw[line, row])
+        plt.imshow(disp_draw)
+
 """___Function______________________________________________________________"""
 
 """___Execution_____________________________________________________________"""
 
 grid = PicrossSolver("mushroom.txt")
 
-print(grid.tips)
-print(grid.line, grid.row)
+grid.solve()
+grid.show()
