7cc18886906180056bb9fbf2d7bb0d0a39787a5a
bwernest <ernest.laprelleboucher@gmail.com>
2025-01-26 20:19:24 +0100

    Methode du decalage

--- PicrossSolver/main.py
+++ PicrossSolver/main.py
@@ -20,6 +20,7 @@ class PicrossSolver() :
     nb_color : int
     tips : dict[str:list[str]] = {}
     draw : np.array
+    done_keys : list[str] = []
 
     def __init__(self, path:str) -> None :
         self.import_from_txt(path)
@@ -43,15 +44,37 @@ class PicrossSolver() :
                 self.row += 1
 
     def solve(self) -> None :
-        self.easy_tips()
-
-    def easy_tips(self) -> None :
         for key, tip in self.tips.items() :
             tip_length = self.get_tip_length(tip)
-            if key[0] == "L" and tip_length == self.line :
+
+            # Lignes évidentes
+            if tip_length == self.line or tip_length == self.row :
                 self.fill_line(key, tip)
-            elif key[0] == "R" and tip_length == self.row :
-                self.fill_line(key, tip)
+            
+            # Méthode décalage
+            max_bloc = max([int(eval(tip[i][1:])) for i in range(len(tip))])
+            shift = self.line if key[0] == "L" else self.row
+            shift -= tip_length
+            if max_bloc > shift and key not in self.done_keys :
+                self.shift(key, tip, shift)
+
+    def shift(self, key:str, tip:str, shift:int) -> None :
+        start = shift
+        for b, bloc in enumerate(tip) :
+            if int(eval(bloc[1:])) > shift :
+                fillable = int(eval(bloc[1:])) - shift
+                if key[0] == "L" :
+                    self.draw[int(key[1:])-1, start:start+fillable] = bloc[0]
+                elif key[0] == "R" :
+                    self.draw[start:start+fillable, int(key[1:])-1] = bloc[0]
+                start += fillable + 1
+
+            else :
+                start += int(eval(bloc[1:]))
+            
+            if b < len(tip)-1 :
+                if tip[b][0] == tip[b+1][0] :
+                    start += 1
 
     def get_tip_length(self, tip:str) -> int :
         lenT = len(tip)
@@ -71,17 +94,24 @@ class PicrossSolver() :
             for i in range(shift, length+shift) :
                 line[i] = color
             shift += length + 1
+        line[line == ""] = "X"
         if key[0] == "L" :
-            self.draw[int(key[1:])] = line
+            self.draw[int(key[1:])-1] = line
         elif key[0] == "R" :
-            self.draw[:, int(key[1:])] = line
+            self.draw[:, int(key[1:])-1] = line
+        self.done_keys.append(key)
     
     def show(self) -> None :
         disp_draw = np.zeros((self.line, self.row), dtype=int)
+        colors = []
         for line in range(self.line) :
             for row in range(self.row) :
                 if self.draw[line, row] != "" :
-                    disp_draw[line, row] = ord(self.draw[line, row])
+                    color = ord(self.draw[line, row])
+                    disp_draw[line, row] = color
+                    if color not in colors : colors.append(color)
+        for c in range(len(colors)) :
+            disp_draw[disp_draw == colors[c]] = c + 1
         plt.imshow(disp_draw)
 
 """___Function______________________________________________________________"""
